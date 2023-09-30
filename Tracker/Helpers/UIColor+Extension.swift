6312185687ddc21d4ee723501519ab24ded0acdc
Eldar Aidumov <superaydumov@me.com>
2023-09-30 20:00:43 +0300

    Added colors in Assets, created LaunchScreen according to Figma template.

---
+++ Tracker/Helpers/UIColor+Extension.swift
@@ -0,0 +1,42 @@
+//
+//  UIColor+Extension.swift
+//  Tracker
+//
+//  Created by Эльдар Айдумов on 30.09.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static var trackerBackgroundDay: UIColor! { UIColor(named: "tracker_Background_Day")}
+    static var trackerBackgroundNight: UIColor! { UIColor(named: "tracker_Background_Night")}
+    static var trackerBlackDay: UIColor! { UIColor(named: "tracker_Black_Day")}
+    static var trackerBlackNight: UIColor! { UIColor(named: "tracker_Black_Night")}
+    static var trackerBlue: UIColor! { UIColor(named: "tracker_Blue")}
+    static var trackerGray: UIColor! { UIColor(named: "tracker_Gray")}
+    static var trackerLightGray: UIColor! { UIColor(named: "tracker_Light_Gray")}
+    static var trackerRed: UIColor! { UIColor(named: "tracker_Red")}
+    static var trackerWhiteDay: UIColor! { UIColor(named: "tracker_White_Day")}
+    static var trackerWhiteNight: UIColor! { UIColor(named: "tracker_White_Night")}
+}
+
+extension UIColor {
+    static var colorSelection1: UIColor! { UIColor(named: "color_Selection_1")}
+    static var colorSelection2: UIColor! { UIColor(named: "color_Selection_2")}
+    static var colorSelection3: UIColor! { UIColor(named: "color_Selection_3")}
+    static var colorSelection4: UIColor! { UIColor(named: "color_Selection_4")}
+    static var colorSelection5: UIColor! { UIColor(named: "color_Selection_5")}
+    static var colorSelection6: UIColor! { UIColor(named: "color_Selection_6")}
+    static var colorSelection7: UIColor! { UIColor(named: "color_Selection_7")}
+    static var colorSelection8: UIColor! { UIColor(named: "color_Selection_8")}
+    static var colorSelection9: UIColor! { UIColor(named: "color_Selection_9")}
+    static var colorSelection10: UIColor! { UIColor(named: "color_Selection_10")}
+    static var colorSelection11: UIColor! { UIColor(named: "color_Selection_11")}
+    static var colorSelection12: UIColor! { UIColor(named: "color_Selection_12")}
+    static var colorSelection13: UIColor! { UIColor(named: "color_Selection_13")}
+    static var colorSelection14: UIColor! { UIColor(named: "color_Selection_14")}
+    static var colorSelection15: UIColor! { UIColor(named: "color_Selection_15")}
+    static var colorSelection16: UIColor! { UIColor(named: "color_Selection_16")}
+    static var colorSelection17: UIColor! { UIColor(named: "color_Selection_17")}
+    static var colorSelection18: UIColor! { UIColor(named: "color_Selection_18")}
+}
