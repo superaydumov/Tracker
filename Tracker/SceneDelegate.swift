13886e6de918f945ef0b570de3ff91222af22085
Eldar Aidumov <superaydumov@me.com>
2023-10-25 21:04:15 +0300

    Created Tracker data model to store database model. Added DatabaseManager class, moved persistentContainer property and saveContext method in this class. refactored AppDelegate and SceneDelegate classes.

--- Tracker/SceneDelegate.swift
+++ Tracker/SceneDelegate.swift
@@ -11,7 +11,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
@@ -46,14 +45,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-
-        // Save changes in the application's managed object context when the application transitions to the background.
-        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+        DatabaseManager.shared.saveContext()
     }
-
-
 }
-
