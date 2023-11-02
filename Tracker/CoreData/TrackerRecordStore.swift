706d26b07e9ef47560febe6720cbb8a6c1e7f5d1
Eldar Aidumov <superaydumov@me.com>
2023-11-03 01:37:09 +0300

    Added DatabaseError enumeration to handle database errors. Refactored TrackerStore and TrackerRecordStore classes (added methods to work with CoreData, fetch and save data).

--- Tracker/CoreData/TrackerRecordStore.swift
+++ Tracker/CoreData/TrackerRecordStore.swift
@@ -9,6 +9,9 @@ import Foundation
 import CoreData
 
 final class TrackerRecordStore {
+    
+    static let shared = TrackerRecordStore()
+    
     private let context: NSManagedObjectContext
     
     convenience init() {
@@ -18,5 +21,40 @@ final class TrackerRecordStore {
     
     init(context: NSManagedObjectContext) {
         self.context = context
+    }
+    
+    func addNewTracker(_ trackerRecord: TrackerRecord) throws {
+        let trackerRecordCoreData = TrackerRecordCoreData(context: context)
+        
+        trackerRecordCoreData.id = trackerRecord.trackerID
+        trackerRecordCoreData.date = trackerRecord.date
+        
+        try context.save()
+    }
+    
+    func deleteTrackerRecord(_ trackerRecord: TrackerRecord) throws {
+        let trackerRecordCoreData = TrackerRecordCoreData(context: context)
+        
+        trackerRecordCoreData.id = trackerRecord.trackerID
+        trackerRecordCoreData.date = trackerRecord.date
+    }
+    
+    func trackerRecord(from data: TrackerRecordCoreData) throws -> TrackerRecord {
+        guard
+            let id = data.id,
+            let date = data.date
+        else { throw DatabaseError.databaseError }
+        
+        return TrackerRecord(
+            trackerID: id,
+            date: date
+        )
+    }
+    
+    func fetchTrackers() throws -> [TrackerRecord] {
+        let fetchRequest = NSFetchRequest<TrackerRecordCoreData>(entityName: "TrackerRecordCoreData")
+        let trackerRecordFromCoreData = try context.fetch(fetchRequest)
+        
+        return try trackerRecordFromCoreData.map { try self.trackerRecord(from: $0) }
     }
 }
---
+++ Tracker/Models/DatabaseError.swift
@@ -0,0 +1,12 @@
+//
+//  DatabaseError.swift
+//  Tracker
+//
+//  Created by Эльдар Айдумов on 03.11.2023.
+//
+
+import Foundation
+
+enum DatabaseError: Error {
+    case databaseError
+}
